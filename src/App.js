4a80821f4211e4462df411ee21a9b4ef82afac17
Raphael <paiva.raphaelt@gmail.com>
2019-11-19 01:20:14 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {sampleText} from './sampleText'
+import Marked from 'marked'
+import Purify from 'dompurify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {contenu: sampleText}
+  //oldLocalStorage = localStorage.setItem('mark', this.state.contenu)
+
+  onChange = (event) => {
+    this.setState({contenu : event.target.value})
+    
+  }
+
+  componentDidMount = () => {
+    if(localStorage.getItem('mark') == null) {
+      localStorage.setItem('mark', this.state.contenu)
+    }
+    if(localStorage.getItem('mark') === ''){
+      localStorage.setItem('mark', this.state.contenu)
+    }
+    if(localStorage.getItem('mark')){
+      this.setState({contenu: localStorage.getItem('mark')})
+    }
+      document.getElementById('mark').innerHTML = Purify.sanitize(Marked(localStorage.getItem('mark')))
+  }
+
+  componentDidUpdate = () => {
+      localStorage.setItem('mark',this.state.contenu)
+      document.getElementById('mark').innerHTML = Purify.sanitize(Marked(localStorage.getItem('mark')))
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea value={this.state.contenu} onChange={this.onChange} className="form-control" rows="30">
+            </textarea>
+          </div>
+          <div id='mark' className="col-sm-6">
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
